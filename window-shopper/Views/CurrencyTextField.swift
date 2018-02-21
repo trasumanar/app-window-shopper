c8c5616cc55b614422fde0ff3fe6735f1dcf5d49
Pascha <atom.tomcat@gmail.com>
2018-02-21 21:09:34 +0300

    created custom text field class

---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Pascha on 21.02.2018.
+//  Copyright © 2018 Pascha. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2515129842)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+    
+
+}
