4ea20934982b9c8331877f4161d818228ecd9910
Pascha <atom.tomcat@gmail.com>
2018-02-22 13:27:43 +0300

    addSubview as label, local currency, formatted strings.. finished the app

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -19,6 +19,22 @@ class CurrencyTextField: UITextField {
     }
     */
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height) / 2 - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9253895879, green: 0.9255481362, blue: 0.9253795743, alpha: 0.3543133803)
+        currencyLbl.textAlignment = . center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true // cause draw deletes defaults
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -37,6 +53,7 @@ class CurrencyTextField: UITextField {
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
+        clipsToBounds = true
     }
     
 
